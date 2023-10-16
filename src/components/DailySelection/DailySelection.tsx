ea074805d072eb6158cbdf026fc0a13418ad4c8a
Michael Kim Sing <mkimsing95@gmail.com>
2023-10-15 18:43:41 -0700

    Update UI and fix button styles

--- src/components/DailySelection/DailySelection.tsx
+++ src/components/DailySelection/DailySelection.tsx
@@ -2,34 +2,43 @@ import Image from "next/image";
 import React from "react";
 import Typography from "@mui/material/Typography";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Link from "next/link";
 export default function DailySelection() {
   return (
-    <div className="flex items-center shadow-md bg-white rounded-lg border-black border-1 py-4 px-6">
-      <div>
-        <Typography variant="h5" className="mb-4">
-          Monday
-        </Typography>
-        <div className="flex items-center">
-          <Image
-            src="https://picsum.photos/200/300"
-            alt="food thumbnail"
-            width="50"
-            height="50"
-            style={{
-              objectFit: "cover",
-              width: 50,
-              height: 50,
-            }}
-          />
-          <div className="mx-8">
-            <Typography variant="subtitle2">Chicken Pesto Pasta</Typography>
-            <Typography variant="body1">
-              Here is some description of a food item
-            </Typography>
+    <Link
+      href="/monday"
+      style={{
+        color: "inherit",
+        textDecoration: "inherit",
+      }}
+    >
+      <div className="flex items-center shadow-md bg-white rounded-lg border-black border-1 py-4 px-6">
+        <div>
+          <Typography variant="h5" className="mb-4">
+            Monday
+          </Typography>
+          <div className="flex items-center">
+            <Image
+              src="https://picsum.photos/200/300"
+              alt="food thumbnail"
+              width="50"
+              height="50"
+              style={{
+                objectFit: "cover",
+                width: 50,
+                height: 50,
+              }}
+            />
+            <div className="mx-8">
+              <Typography variant="subtitle2">Chicken Pesto Pasta</Typography>
+              <Typography variant="body1">
+                Here is some description of a food item
+              </Typography>
+            </div>
           </div>
         </div>
+        <ArrowForwardIosIcon fontSize="medium" />
       </div>
-      <ArrowForwardIosIcon fontSize="medium" />
-    </div>
+    </Link>
   );
 }
